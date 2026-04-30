d5cbd34dd3311223ea1a3c6cfddf3f9e9e574b6e
robertobandiera-png <roberto.bandiera@pitagorapolicoro.eu>
2026-04-30 11:43:16 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'epidemix-v1';
+const assets = [
+  'index.html',
+  'manifest.json',
+  'icona.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
